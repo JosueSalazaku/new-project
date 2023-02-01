bfaa467813572d39b32836d586fa2b30fe06b984
JosueSalazaku <95032713+JosueSalazaku@users.noreply.github.com>
2023-02-02 00:13:34 +0100

    init

---
+++ script.js
@@ -0,0 +1,6 @@
+const time = document.querySelector("#time")
+let date = new Date();
+let hours = date.getHours();
+let minutes = date.getMinutes();
+let seconds = date.getSeconds();
+time.textContent = hours + ":" +  minutes + ":" + seconds;
